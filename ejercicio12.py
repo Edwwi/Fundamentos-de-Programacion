3bea55a2335bb0c8fdef9362e0265132a5c861c9
Edwi C <93277139+Edwwi@users.noreply.github.com>
2024-06-03 19:46:24 -0400

    Arreglo
    Agregue sumar los digitos si es par y quite los while innecesarios

--- ejercicio12.py
+++ ejercicio12.py
@@ -3,26 +3,36 @@ print("Juego (Ejercicio 12): \nMe daras dos numeros de esos sacare su diferencia
 primerNumero = int(input("Ingrese el primer numero: "))
 segundoNumero = int(input("Ingrese el segundo numero: "))
 
-numerosPrimos = [2, 3, 5, 7] # Numeros primos
+numerosPrimos = [2, 3, 5, 7] # Numeros primos menor 10
 
-resultado = abs(primerNumero - segundoNumero)
+if primerNumero and segundoNumero <= 0 or primerNumero and segundoNumero >= 100 or primerNumero and segundoNumero < 10:
+    
+    quit("El numero dado es mayor a 2 digitos o es negativo.")
 
+else:
+    resultado = abs(primerNumero - segundoNumero)
 
-if resultado %2 == 0: #Identificar si un numero es par o no con el modulo 2
-    while resultado != 0:
-        print("El numero es un numero par: \n» La suma de los numeros es: ", primerNumero%10 + segundoNumero%10)
-        break
-elif resultado in numerosPrimos: # Verificar que esta en la lista de los numeros primos
-    print("Este numero es un numero primo menor a 10 \n» La multiplicacion de los numeros es: ", primerNumero*segundoNumero)
-elif resultado%10 == 4: # Si el resultado termina en 4 muestra los digitos por seperado
-    while resultado != 0:
+    if resultado %2 == 0: #Identificar si un numero es par o no con el modulo 2
+        primerDigito = int(primerNumero/10) # Si divido entre 10 me da el primer digito
+        segundoDigito = primerNumero%10 # Si pongo modular 10 me da segundo digito
+        tercerDigito = int(segundoNumero/10)
+        cuartoDigito = segundoNumero%10
+        print("El numero es un numero par: \n» La suma de los digitos es: ", primerDigito + segundoDigito + tercerDigito + cuartoDigito)
+    else:
+        print("El resultado no es un numero par...")
+
+    if resultado in numerosPrimos: # Verificar que esta en la lista de los numeros primos
+        print("Este numero es un numero primo menor a 10 \n» La multiplicacion de los numeros es: ", primerNumero*segundoNumero)
+    else:
+        print("El resultado no es un numero primo...")
+    if resultado%10 == 4: # Si el resultado termina en 4 muestra los digitos por seperado
         print("La diferencia de los numeros", primerNumero, "y ", segundoNumero, " terminan en 4\nte mostrare los digitos por separado de cada numero:")
-        
-        print("Digitos del primero numero ({primerNumero}): ")
+    
+        print("Digitos del primero numero (", primerNumero, "): ")
         for digitos in str(primerNumero):
             print(digitos)
-        print("Digitos del primero numero ({segundoNumero}): ")
+        print("Digitos del primero numero (", segundoNumero, "): ")
         for digitos in str(segundoNumero):
             print(digitos)
-else:
-    print("El resultado no cumple con ninguno de los requisitos")
+    else:
+        print("El numero no termina en 4...")*
