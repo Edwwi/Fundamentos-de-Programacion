671f527229a869d2b0bb410e6442992f1ac5c01d
Edwi <edwicitoo@gmail.com>
2024-06-11 12:58:53 -0400

    Tercera parte terminada

---
+++ proyectoltd3.py
@@ -0,0 +1,15 @@
+numeros = [25, 30, 40, 50, 75]
+print("La lista es: ",numeros)
+print("Primera posicion: ", numeros[0])
+print("Segunda posicion: ", numeros[4])
+
+numeros[3] = 40
+print(numeros)
+
+equipo = {"Equipo" : "Dallas Mavericks", "JugadorE" : "Luka Doncic", "Puntos" : 20.4}
+print(equipo)
+print("Los puntos del jugador estrella son: ", equipo["Puntos"])
+
+equipo["JugadorE"] = "Kyrie Irving"
+
+print(equipo)
