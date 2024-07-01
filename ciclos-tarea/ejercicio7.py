bf249b9ff85c3b85e1be7586d9b5f41b468c5523
Edwi <edwicitoo@gmail.com>
2024-07-01 10:45:41 -0400

    Ejercicio 7 terminado

---
+++ ciclos-tarea/ejercicio7.py
@@ -0,0 +1,7 @@
+# 7. Mostrar en pantalla todos los enteros comprendidos entre 1 y 100.
+
+primerNumero = 1
+segundoNumero = 100
+
+for enteros in range(primerNumero, segundoNumero+1):
+    print(enteros, end=", ")
