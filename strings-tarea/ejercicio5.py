3eee6f781d4810052014e175ed4a33454818a1ca
Edwi <edwicitoo@gmail.com>
2024-07-04 10:13:27 -0400

    Ejercicio 5 terminado sobre strings

---
+++ strings-tarea/ejercicio5.py
@@ -0,0 +1,9 @@
+# Leer un texto y realizar las siguientes operaciones: Retornar los dos últimos caracteres del texto
+
+
+
+ingresa = "Ingresa cualquier texto" # 20
+
+
+
+print(f"La letra {ingresa[22:23]} se repite: {ingresa.count("o")} vez")
