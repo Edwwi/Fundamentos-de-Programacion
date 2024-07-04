77aa4de79e3769dd2f294d11be2ff27d04eb0719
Edwi <edwicitoo@gmail.com>
2024-07-04 10:10:36 -0400

    Ejercicio 4 terminado sobre strings

---
+++ strings-tarea/ejercicio4.py
@@ -0,0 +1,9 @@
+# Leer un texto y realizar las siguientes operaciones: Retornar los dos últimos caracteres del texto
+
+
+
+ingresa = "Ingresa cualquier texto" # 20
+
+
+
+print(f"{ingresa[21:23]}")
