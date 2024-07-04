7a73597f1292536c5fa31a59d70ed89fabf66dbc
Edwi <edwicitoo@gmail.com>
2024-07-04 10:05:08 -0400

    Ejercicio 3 terminado sobre strings

---
+++ strings-tarea/ejercicio3.py
@@ -0,0 +1,8 @@
+# Leer un texto y realizar las siguientes operaciones: Retornar los dos primeros caracteres del texto
+
+
+
+ingresa = input("Ingresa cualquier texto \n» ")
+
+
+print(f"{ingresa[0:2]}")
