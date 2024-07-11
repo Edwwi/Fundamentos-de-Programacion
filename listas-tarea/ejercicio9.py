2ddf93bd46db152d80f77cdf552fc6ec3c2e031d
Edwi <edwicitoo@gmail.com>
2024-07-10 23:52:55 -0400

    Ejercicio 9 terminado

--- listas-tarea/ejercicio9.py
+++ listas-tarea/ejercicio9.py
@@ -2,18 +2,17 @@ import math
 
 lista = []
 
-# Leer 10 números enteros
 for contador in range(10):
     pedir = int(input(f"Ingrese el número #{contador + 1}: "))
     lista.append(pedir)
 
 print(f"La lista resultante es: {lista}")
 
-# Lista para almacenar los factoriales
+
 listaFactoriales = []
 
 for numero in lista:
-    factorial = math.factorial(numero)  # Usar math.factorial para calcular el factorial
+    factorial = math.factorial(numero)  
     listaFactoriales.append(factorial)
 
 print(f"La lista de factoriales es: {listaFactoriales}")
