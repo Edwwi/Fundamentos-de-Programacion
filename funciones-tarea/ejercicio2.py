012692718d7d9265893f13bc844afa4aca5a4d7a
Edwi <edwicitoo@gmail.com>
2024-07-22 10:15:19 -0400

    Ejercicio 2 sobre funciones terminado

---
+++ funciones-tarea/ejercicio2.py
@@ -0,0 +1,6 @@
+def suma(a, b):
+    operacion = a + b
+    return operacion
+
+llamada = suma(a = int(input("Primer numero para sumar: ")), b = int(input("Segundo numero para sumar: ")))
+print(llamada)
